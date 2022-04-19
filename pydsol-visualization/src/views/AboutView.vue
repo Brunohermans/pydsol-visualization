8a8c8412760019a6d6615e98cc1c19411bee3504
Brunohermans <47179528+Brunohermans@users.noreply.github.com>
2022-04-19 15:00:17 +0200

    added map to show structure

---
+++ pydsol-visualization/src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Map from "@/components/Map.vue";
+</script>
+
+
+<template>
+  <div class="about">
+    <h1>Pydsol visualization</h1>
+    <Map />
+  </div>
+</template>
+
+<style>
+
+</style>
